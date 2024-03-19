4f06c31076ee6be5f5e884b71e4a14a23503abb6
LFAllard <fredrik.allard@axello.se>
2024-03-19 10:19:55 +0100

    på gång med merge

--- App.js
+++ App.js
@@ -1,64 +1,50 @@
-//XXXXXXXXXXX Boilerplate från react native
-
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-//XXXXXXXXXXXXX Principskiss från ChatGPT
-
-import React from "react";
-import { Provider } from "react-redux";
-import store from "./redux/store/ConfigureStore";
-import { NavigationContainer } from "@react-navigation/native";
-import StackNavigator from "./navigation/AppNavigator";
-
-function App() {
-  return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
-    </Provider>
-  );
-}
-
-export default App;
-
-//XXXXXXXXXXXXXXX Från Fixmapp
-
-// import * as React from "react";
-// import { StatusBar } from "expo-status-bar";
-// import { StyleSheet, Text, View, Button } from "react-native";
+// import React from "react";
+// import { Provider } from "react-redux";
+// import store from "./redux/store/ConfigureStore";
 // import { NavigationContainer } from "@react-navigation/native";
-// import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-// // import Kf from "./app/screens/KfOoo";
-
 // import StackNavigator from "./navigation/AppNavigator";
 
 // function App() {
 //   return (
-//     <NavigationContainer>
-//       <StackNavigator />
-//     </NavigationContainer>
+//     <Provider store={store}>
+//       <NavigationContainer>
+//         <StackNavigator />
+//       </NavigationContainer>
+//     </Provider>
 //   );
 // }
 
 // export default App;
+
+//Försök med dispatch av load open dataXXXXXXXXXXXXXXXXXXXXXXXX
+
+import React, { useEffect } from "react";
+import { Provider, useDispatch } from "react-redux";
+import store from "./redux/store/ConfigureStore";
+import { NavigationContainer } from "@react-navigation/native";
+import StackNavigator from "./navigation/AppNavigator";
+import { loadOpenData } from "./redux/store/slices/openData";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <AppContent />
+    </Provider>
+  );
+}
+
+function AppContent() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadOpenData()); // Dispatch your action
+  }, [dispatch]);
+
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+}
+
+export default App;
--- redux/store/ConfigureStore.js
+++ redux/store/ConfigureStore.js
@@ -38,6 +38,8 @@
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
+//DENNA FUNKAR MEN ÄR UTAN DEBUGGING...
+
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
@@ -54,3 +56,144 @@ const store = configureStore({
 });
 
 export default store;
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+// import { composeWithDevTools } from "remote-redux-devtools";
+// import reducer from "./reducer";
+// import logger from "./middleware/logger";
+// import toast from "./middleware/toast";
+// import api from "./middleware/api";
+
+// const store = configureStore({
+//   reducer,
+//   middleware: (getDefaultMiddleware) =>
+//     composeWithDevTools(
+//       getDefaultMiddleware()
+//         .concat(logger({ destination: "console" }))
+//         .concat(toast)
+//         .concat(api)
+//     ),
+// });
+
+// export default store;
+
+//XXXXXXXXXXXXXXXXXXXXXXXX Nästa försök med @redux-devtools/extension 240304
+
+// import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+// import { composeWithDevTools } from "@redux-devtools/extension";
+// import reducer from "./reducer";
+// import logger from "./middleware/logger";
+// import toast from "./middleware/toast";
+// import api from "./middleware/api";
+
+// const store = configureStore({
+//   reducer,
+//   middleware: (getDefaultMiddleware) =>
+//     composeWithDevTools(
+//       getDefaultMiddleware()
+//         .concat(logger({ destination: "console" }))
+//         .concat(toast)
+//         .concat(api)
+//     ),
+// });
+
+// export default store;
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Nästa försök 240304
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import reducer from "./reducer";
+// import logger from "./middleware/logger";
+// import toast from "./middleware/toast";
+// import api from "./middleware/api";
+
+// const store = configureStore({
+//   reducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware()
+//       .concat(logger({ destination: "console" }))
+//       .concat(toast)
+//       .concat(api),
+// });
+
+// export default store;
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import { composeWithDevTools } from "@redux-devtools/remote";
+// import reducer from "./reducer";
+// import logger from "./middleware/logger";
+// import toast from "./middleware/toast";
+// import api from "./middleware/api";
+
+// const composedEnhancer = composeWithDevTools({
+//   realtime: true, //REMOVE IN PRODUCTION!!
+//   hostname: "localhost", //REMOVE IN PRODUCTION!!
+//   port: 8000, //REMOVE IN PRODUCTION!!
+// });
+
+// const store = configureStore({
+//   reducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware()
+//       .concat(logger({ destination: "console" }))
+//       .concat(toast)
+//       .concat(api),
+//   enhancers: [composedEnhancer],
+// });
+
+// export default store;
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX nu 240306
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import { composeWithDevTools } from "@redux-devtools/remote";
+// import reducer from "./reducer";
+// import logger from "./middleware/logger";
+// import toast from "./middleware/toast";
+// import api from "./middleware/api";
+
+// const composedEnhancer = composeWithDevTools({
+//   realtime: true,
+//   hostname: "localhost",
+//   port: 8000,
+// });
+
+// const store = configureStore({
+//   reducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware()
+//       .concat(logger({ destination: "console" }))
+//       .concat(toast)
+//       .concat(api),
+//   enhancers: (defaultEnhancers) => [composedEnhancer(...defaultEnhancers)],
+// });
+
+// export default store;
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Ännu ett försök 240306
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import { composeWithDevTools } from "@redux-devtools/remote";
+// import reducer from "./reducer";
+// import logger from "./middleware/logger";
+// import toast from "./middleware/toast";
+// import api from "./middleware/api";
+
+// const composedEnhancer = composeWithDevTools({
+//   realtime: true,
+//   hostname: "localhost",
+//   port: 8000,
+// });
+
+// const store = configureStore({
+//   reducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware().concat(logger).concat(toast).concat(api),
+//   enhancers: (defaultEnhancers) => [composedEnhancer(...defaultEnhancers)],
+// });
+
+// export default store;
